349d323b11eb9fb93d5f15bb614d15586fee4547
khang <khangnguyen41888@gmail.com>
2024-10-15 22:47:48 +0700

    update teacher upload

--- src/main/java/khang/test/example/demo/repository/TeacherRepository.java
+++
@@ -1,7 +0,0 @@
-package khang.test.example.demo.repository;
-
-import khang.test.example.demo.entity.Teachers;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teachers, Integer> {
-}
